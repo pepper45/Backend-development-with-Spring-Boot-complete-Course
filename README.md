b41be8d51b2fb39b9b2450bf5189488213b5e795
pepper45 <zapper.ethan@gmail.com>
2021-10-20 22:54:33 +0530

    commit message

---
+++ README.md
@@ -0,0 +1,3 @@
+# Backend-development-with-Spring-Boot-complete-Course
+
+Code for backend development with Spring Boot involving REST APIs, AOP, JPA
---
+++ src/main/java/com/example/backend/controller/basic/HelloController.java
@@ -0,0 +1,26 @@
+package com.example.backend.controller.basic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+  private static String serverId;
+  @Autowired ApplicationArguments args;
+
+  @RequestMapping("/world")
+  String home() {
+    return "Hello World";
+  }
+
+  @RequestMapping("/server")
+  public String server(HttpServletRequest request) {
+    return "Hello from server : " + args.getSourceArgs()[0];
+  }
+}
